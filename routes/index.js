061df5fb72459e8adf7f8965b52a79a83505b247
birdsandsky <congthanhcdt10@gmail.com>
2016-10-06 10:49:04 +0700

    first
    ádf

--- routes/index.js
+++ routes/index.js
@@ -4,59 +4,20 @@ var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var myArtwork = [];
-  var myArtists = [];
-
-  myArtists = appdata.speakers;
-  appdata.speakers.forEach(function(item) {
-    myArtwork = myArtwork.concat(item.artwork);
-  });
   res.render('index', {
     title: 'Home',
-    artwork: myArtwork,
-    artists: myArtists,
     page: 'home'
   });
 });
 
 /* GET speakers page. */
-router.get('/speakers', function(req, res) {
-  var myArtwork = [];
-  var myArtists = [];
-  myArtists = appdata.speakers;
 
-  appdata.speakers.forEach(function(item) {
-    myArtwork = myArtwork.concat(item.artwork);
-  });
-  res.render('speakers', {
-    title: 'Speakers',
-    artwork: myArtwork,
-    artists: myArtists,
+router.get('/NganHangNhaTro', function(req, res) {
+  res.render('NganHangNhaTro', {
+    title: 'NganHangNhaTro',
     page: 'artistList'
   });
 });
-
-
-/* GET speakers detail page */
-router.get('/speakers/:speakerid', function(req, res) {
-  var myArtwork = [];
-  var myArtists = [];
-
-  appdata.speakers.forEach(function(item) {
-    if (item.shortname == req.params.speakerid) {
-      myArtists.push(item);
-      myArtwork = myArtwork.concat(item.artwork);
-    }
-  });
-  res.render('speakers', {
-    title: 'Speakers',
-    artwork: myArtwork,
-    artists: myArtists,
-    page: 'artistDetail'
-  });
-});
-
-
 
 
 module.exports = router;
